483e3c25c1b5544905010430b726a97f40be5566
Apolon-as <129028339+Apolon-as@users.noreply.github.com>
2023-06-23 02:00:50 +0600

    final

--- Events.js
+++ Events.js
@@ -3,3 +3,12 @@ window.addEventListener('scroll', function() {
     var parallaxImage = document.querySelector('.parallax-image');
     parallaxImage.style.transform = 'translateY(' + (scrollTop * 0.4) + 'px)';
   });
+
+  <script>
+  var button = document.getElementById("myButton");
+
+  button.addEventListener("click", function() {
+    // Display an alert when the button is clicked
+    alert("Button clicked!")
+  });
+</script>
